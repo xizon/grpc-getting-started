3e83f7ce9c9281de9c56285e17f5275bc10e8055
UIUX Lab <uiuxlab@gmail.com>
2023-04-14 09:19:47 +0800

    update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
 # declare STRING variable
-STRING="Starting node processes"
+STRING_START="--> Starting node processes"
+STRING_RUN="--> All gRPC services are running"
+STRING_END="--> All is ending"
+
+
 #print variable on a screen
-echo $STRING
+echo $STRING_START
 
 node .src/server/example.js&
 node .src/server/post.js&
 
+echo $STRING_RUN
 
 # wait until both background processes finish
 wait
-
-
-
-
-
+echo $STRING_END
